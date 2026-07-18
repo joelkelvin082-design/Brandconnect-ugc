4bd6fae96eb16e2123ea31373164396bae11ed6c
joelkelvin082-design <joelkelvin082@gmail.com>
2026-07-18 08:19:52 +0100

    Update Tailwind.config.js

--- Tailwind.config.js
+++ Tailwind.config.js
@@ -1,5 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  content: [
+    "./*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
