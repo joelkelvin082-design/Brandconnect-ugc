aed7d85ea2fb5c3479bce6ba4de346580d69df18
joelkelvin082-design <joelkelvin082@gmail.com>
2026-07-18 08:17:24 +0100

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-10 text-center">
+      <h1 className="text-4xl font-bold">BrandConnect</h1>
+      <p className="mt-4 text-gray-600">Connect brands with creators</p>
+    </main>
+  )
+}
